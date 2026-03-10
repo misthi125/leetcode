50fb1eaf0048966cbdfb886d16b4cb0a292c107f
misthi125 <shrutimp11@gmail.com>
2026-03-10 14:18:32 +0530

    Time: 0 ms (100%), Space: 8.6 MB (75.91%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int titleToNumber(string ct) {
-      long cnt=1;
-      int ans=0;
-      for(int i=ct.size()-1;i>-1;i--){
-        ans+=(ct[i]-'A'+1)*cnt;
-        cnt=cnt*26;
-      }  
+        int ans=0;
+      for(auto c:ct){
+        ans=ans*26+(c-'A'+1);
+      }
       return ans;
     }
 };
