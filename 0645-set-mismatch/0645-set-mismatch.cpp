511af00582904ad936b4d3b118443e8dc71c60c7
misthi125 <shrutimp11@gmail.com>
2026-06-12 14:48:44 +0530

    Time: 15 ms (33.35%), Space: 25.1 MB (88.71%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-       int a=0,b=0;
-       for(int i=0;i<nums.size();i++){
-        a=a^(i+1);
-        b=b^nums[i];
-       } 
-       int r=a^b;
-       r=r& -r;
-       int s=0,ns=0;
-       for(int i=1;i<=nums.size();i++){
-        if(i&r)s^=i;
-        else ns^=i;
-       }
-       for(auto i:nums){
-        if(i&r)s^=i;
-        else ns^=i;
-       }
-       for( auto i:nums){
-        if(i==s)return {s,ns};
-       }
-       return {ns,s};
+        vector<int>a(2,0);
+        sort(nums.begin(),nums.end());
+        int j=1;
+        for(int i=0;i<nums.size();i++){
+            if(j==nums[i])j++;
+            else{
+                if(nums[i]==j-1)a[0]=nums[i];
+                else {
+                    a[1]=j++;
+                    i--;
+                }
+            }
+        }
+        if(a[1]==0)a[1]=j;
+        return a;
     }
 };
